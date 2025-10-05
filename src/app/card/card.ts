0635030e83cad19b3da5b2372f5ec8eda30a1894
castolo10a <castillod159@gmail.com>
2025-10-04 23:30:52 -0500

    feat: add function formatMiniCardText

--- src/app/card/card.ts
+++ src/app/card/card.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-card',
@@ -14,6 +15,18 @@ export class Card {
   @Input() subtitle!: string;
   @Input() text!: string;
   @Input() miniCardText!: string;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  formatMiniCardText(): SafeHtml {
+    if (!this.miniCardText) return '';
+    // Reemplaza cualquier cifra con $ o % por un span con color
+    const formatted = this.miniCardText.replace(
+      /(\$\d+[\d,.]*|\d+%)/g,
+      `<span style="color:#FE9200">$1</span>`
+    );
+    return this.sanitizer.bypassSecurityTrustHtml(formatted);
+  }
   @Input() miniCardIcon!: string;
   @Input() borderColor!: string;
 }
