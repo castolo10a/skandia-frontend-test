d092478c52f1bce8ff924ccb82e2c6a8e85a82a6
castolo10a <castillod159@gmail.com>
2025-10-05 06:36:18 -0500

    feat: add footer component

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Header } from './header/header';
 import { Sidebar } from './sidebar/sidebar';
 import { HeroSection } from './hero-section/hero-section';
 import { SliderComponent } from './slider/slider';
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Sidebar, HeroSection, SliderComponent],
+  imports: [RouterOutlet, Header, Sidebar, HeroSection, SliderComponent, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
